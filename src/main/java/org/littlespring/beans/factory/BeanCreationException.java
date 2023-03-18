7473dc78e899ebdfdfe59c7b860f6e82d850bee5
tiany <908673267@qq.com>
2023-03-19 00:51:43 +0800

    北纬8° 【将异常实现】

--- src/main/java/org/littlespring/beans/factory/BeanCreationException.java
+++ src/main/java/org/littlespring/beans/factory/BeanCreationException.java
@@ -3,7 +3,7 @@ package org.littlespring.beans.factory;
 import org.littlespring.beans.BeansException;
 
 public class BeanCreationException extends BeansException {
-
+    private String beanName;
 
     public BeanCreationException(String message) {
         super(message);
@@ -11,5 +11,19 @@ public class BeanCreationException extends BeansException {
 
     public BeanCreationException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    public BeanCreationException(String beanName, String message) {
+        super("Error creating bean with name '" + beanName + "' ;" + message);
+        this.beanName = beanName;
+    }
+
+    public BeanCreationException(String beanName, String message, Throwable cause) {
+        this(beanName, message);
+        initCause(cause);
+    }
+
+    public String getBeanName() {
+        return this.beanName;
     }
 }
